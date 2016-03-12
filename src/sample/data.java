6d45e7fcc2cc8987c0f26647ed3f000d2b996335
minh duong <mcd4874@g.rit.edu>
2016-03-12 15:47:40 -0500

    finish user and portfolio create accout

--- src/sample/data.java
+++ src/sample/data.java
@@ -1,4 +1,5 @@
 package sample;
+import javax.sound.sampled.Port;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -20,29 +21,16 @@ public class data  {
     /*
     * save a new account to data
     */
-
     public void saveAccount(User newAccount){
         //check wthether the account has been created or not
         if (!isUserExist(newAccount)) {
             //get the list of User account from the text file
             userList = this.listOfUser();
+            userList.add(newAccount);
             //open the text file and save the new account
-            try {
-                FileOutputStream fileOut = new FileOutputStream("employee.txt");
-                ObjectOutputStream out = new ObjectOutputStream(fileOut);
-                //userList.add(newAccount);h
-                int userID = userList.size();
-                newAccount.setUserID(userID);
-                userList.add(newAccount);
-                out.writeObject(userList);
-                out.close();
-                fileOut.close();
-            } catch (FileNotFoundException i) {
-                i.printStackTrace();
-            } catch (IOException i) {
-                i.printStackTrace();
-            }
-            //System.out.println("Done");
+            this.updateAccountList(userList);
+            //create new PortfolioAccount
+            this.savePortfolioAccount(newAccount.username());
         } else {
             System.out.println("account exists");
 
@@ -50,20 +38,82 @@ public class data  {
     }
 
     /*
-    * pre-condition: the user account is created before the fortfolio account is created
-    * Post-condition: save the Portfolio account into the array in the text file
+  * pre-condition: the user account is created before the fortfolio account is created
+  * Post-condition: save the Portfolio account into the array in the text file
+  * */
+    public void savePortfolioAccount(String username){
+        Portfolio newPortfolio = new Portfolio(username,new ArrayList<Equity>(),new ArrayList<CashAccount>());
+        List<Portfolio> listOfPortfolio = this.listOfPortfolio();
+        listOfPortfolio.add(newPortfolio);
+        this.updatePortfolioList(listOfPortfolio);
+    }
+
+    /*
+    * delete the account and portfolio with the known username
     * */
-    public void savePortfolioAccount(Portfolio p){
+    public void deleteUserAccount(String username){
+        List<User> listOfAccount = this.listOfUser();
+        List<Portfolio> listOfPortfolio = this.listOfPortfolio();
+        for (User user: listOfAccount){
+            if (user.username().equals(username)){
+                listOfAccount.remove(user);
+                break;
+            }
+        }
+
+        for (Portfolio p : listOfPortfolio){
+            if(p.getUserID().equals(username)){
+                listOfPortfolio.remove(p);
+                break;
+            }
+        }
+        this.updateAccountList(listOfAccount);
 
     }
+
+    /*
+    * update the portfolio list in the text file
+    * */
+    public void updatePortfolioList(List<Portfolio> listOfPortfolio){
+        try {
+            FileOutputStream fileOut = new FileOutputStream("portfolio.txt");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(listOfPortfolio);
+            out.close();
+            fileOut.close();
+        } catch (FileNotFoundException i) {
+            i.printStackTrace();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    /**
+     * update the account list in the text file
+     *
+     */
+    public void updateAccountList(List<User> listOfAccount){
+        try {
+            FileOutputStream fileOut = new FileOutputStream("employee.txt");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(listOfAccount);
+            out.close();
+            fileOut.close();
+        } catch (FileNotFoundException i) {
+            i.printStackTrace();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+
+
 
     /*
     * check if the user has made an account before
     * */
     public boolean isUserExist(User checkUser){
-
         return this.listOfUser().contains(checkUser);
-
     }
 
     /*c
@@ -72,11 +122,32 @@ public class data  {
     public List<User> listOfUser (){
         List<User> listOfAccount = new ArrayList<User>();
         //access the text file employee.text
+        Object temporary = this.listOfObject("employee.txt");
+        if (temporary != null){
+            listOfAccount = (ArrayList<User>) temporary;
+        }
+
+        return listOfAccount;
+    }
+
+    public List<Portfolio> listOfPortfolio (){
+        List<Portfolio> listOfPortfolio = new ArrayList<Portfolio>();
+        //access the text file employee.text
+        Object temporary = this.listOfObject("portfolio.txt");
+        if (temporary != null){
+            listOfPortfolio = (ArrayList<Portfolio>) temporary;
+        }
+
+        return listOfPortfolio;
+    }
+
+    public Object listOfObject(String text){
+        Object list= null;
         try {
-            FileInputStream fileOut = new FileInputStream("employee.txt");
+            FileInputStream fileOut = new FileInputStream(text);
             if(fileOut.available() > 0) {
                 ObjectInputStream is = new ObjectInputStream(fileOut);
-                listOfAccount = (ArrayList<User>) is.readObject();
+                list = is.readObject();
                 is.close();
             }
         } catch (FileNotFoundException i){
@@ -88,7 +159,7 @@ public class data  {
             i.printStackTrace();
         }
 
-        return listOfAccount;
+        return list;
     }
 
 
